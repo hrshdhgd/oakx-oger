c715b53eda249aa5271f100ef20d3f7864b69692
Harshad Hegde <hegdehb@gmail.com>
2022-10-28 08:58:38 -0500

    Undoing #4

--- src/oakx_oger/_version.py
+++
@@ -1,9 +0,0 @@
-"""Auto versioning code."""
-from importlib.metadata import PackageNotFoundError, version
-
-"""Get version or set it to 0.0.0."""
-try:
-    __version__ = version("oakx-oger")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "0.0.0"
