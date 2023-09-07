390a74960ea056ff8ea7eba2de5dcb9dea287fc8
Harshad Hegde <hegdehb@gmail.com>
2023-09-07 11:12:03 -0500

    all tests pass

--- src/oakx_oger/__init__.py
+++ src/oakx_oger/__init__.py
@@ -3,8 +3,8 @@ try:
     from importlib import metadata
 except ImportError:
     # Python < 3.8
-    import importlib_metadata as metadata  # pragma: no cover
-                
+    import importlib_metadata as metadata  # type: ignore
+
 try:
     __version__ = metadata.version(__name__)
 except metadata.PackageNotFoundError:
